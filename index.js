1b3135242ff129432e07c75e3e524b6d0e95bbf3
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-18 16:13:19 -0300

    setando evento onclick e função no button html, corrigindo nome de class no ccs, codificando index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function trocarCorPrimeiroQuadrado(){
+    let quadradoSelecionado = document.getElementsByClassName("quadrado")[0];
+    quadradoSelecionado.classList.add("nova-cor");
+    console.log(quadradoSelecionado.classList)
+}   
